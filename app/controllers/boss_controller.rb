617a8fcf0fbfb08a95ca011676580b857bcc66ea
Arturo Pacifico Griffini <pacifico.arturo@gmail.com>
2014-07-10 15:00:12 -0700

    add controller implementation of boss

--- app/controllers/boss_controller.rb
+++ app/controllers/boss_controller.rb
@@ -4,7 +4,11 @@ class BossController < ApplicationController
     query = params["search"]
 
     #BOSS stuff----
+    boss = BOSSMan::Search.web("prospect park", :count => 10, :filter => "-hate")
 
+    boss.results.each do |result|
+      urls << result.url
+    end
     #--------------
 
     urls = ["http://en.wikipedia.org/wiki/Arturo"]
