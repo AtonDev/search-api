fa94d2c6ff587912bc16d20d399440ddfa399ea2
User <user@ubuntu.(none)>
2014-07-26 10:06:18 -0700

    Add ability to get full results from boss

--- app/controllers/boss_controller.rb
+++ app/controllers/boss_controller.rb
@@ -19,5 +19,24 @@ class BossController < ApplicationController
     render :json => {status: 'ok'}
   end
 
+  def get_boss_full_results
+    query = params["search"]
+
+    unless query == ""
+      urls = []
+      dispurls = []
+      titles = []
+      abstracts = []
+      res = YBoss.web('q' => query)
+      res.items.each do |i|
+        urls << i.url
+        dispurls << i.dispurl
+        titles << i.title
+        abstracts << i.abstract 
+      end
+    end
+    #--------------
+    render :json => {urls: urls}
+  end
 
 end
